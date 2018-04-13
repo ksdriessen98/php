3a236dd0aa11fee051405561fd1d79991cd62449
keanu driessen <keanu.shawn@hotmail.com>
2018-04-13 11:16:44 +0200

    opdracht 2 gemaakt.

---
+++ opdracht 2/opdracht 2.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: keanu driessen
+ * Date: 4/13/2018
+ * Time: 11:16 AM
+ */
+$aantal = 14;
+$deze_jongen = "Deze jongen is ";
+echo $deze_jongen.$aantal." jaar oud!";
